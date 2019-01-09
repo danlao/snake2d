9c467498078c616cfc107e6c0e168f2437d0d198
Daniel Lao <dan.lao@outlook.com>
2019-01-08 22:04:52 -0600

    CLEAN up existing code; added start menu with options for different difficulty levels, added score display and calculation based on difficulty, added proper game over screen, added option to PLAY AGAIN upon GAME OVER

--- snake2d.js
+++ snake2d.js
@@ -1,209 +1,261 @@
+var LEVEL = {
+    EASY: 6,
+    MEDIUM: 15,
+    HARD: 30,
+    INSANE: 60
+};
 
-/***************************** VARIABLE DECLARATION/INITIALIZATION *****************************/
-
-//  canvas: HTML canvas
-//  painter: canvas context
-//  snek: array of positions IN REVERSE ORDER
-//        mainly pop-tail and push-head operations, so reverse array has better optimized oprn's
-//          [ {x: x_, y: y_}
-//           ...  
-//          {x: x0, y: y0} ]
-//  food: {
-//      x: food X pos
-//      y: food Y pos
-//  }
 var canvas, painter;
-var food, foodEaten;
-var snek, snek_len, speedX, speedY, growSize, growCnt;
+var food, isFoodEaten;
+var snek, speedX, speedY, growFramesCounter;
 
 var blockSize = 20;    
-var snakeFill = 'white';
-var snakeFillHead = 'pink';
-var snakeFillTail = 'brown';
-var foodFill = 'red';
-var timeNow = 0;
-var gameOver = false;
+var snekBodyColor = 'white';
+var snekHeadColor = 'pink';
+var snekTailColor = 'brown';
+var foodColor = 'red';
+var score = 0;
+var isGameOver = false;
 
-/************************************** CLASS DEFINITIONS **************************************/
+var growSegments = 2;
 
-class SlistNode {
+var difficulty = null;
+var resetGame = false;
+
+class SnekBodySegment {
     
-    constructor(val) {
-        this.value = val || null;
-        this.prev = null;
-        this.next = null;
+    constructor(intialCoordinatePosition) {
+        if (!intialCoordinatePosition) {
+            //TODO: throw something here
+        }
+        
+        this.coordinatePosition = intialCoordinatePosition || null;
+        this.previousSegment = null;
+        this.nextSegment = null;
     }
-
+    
 }
 
-class SlinkedSlist {
+class SnekBody {
     
-    constructor(node) {
-        
+    constructor(initialBodySegment) {
         this.head = null;
         this.tail = null;
         
-        if (node) {
-            this.head = node;
-            this.tail = node;
-            node.prev = null;
-            node.next = null;
+        if (!initialBodySegment) {
+            //TODO: throw something here
         }
-    }
-
-    // state when empty
-    // add to 0, 1, 1+
-    pushHead(node) {
-
-        if (this.head === null) {
-            node.prev = null;
-            node.next = null;
-            this.head = node;
-            this.tail = node;
-        } else {
-            node.prev = null;
-            node.next = this.head;
-            this.head.prev = node;
-            this.head = node;
-        }
-
-    }
-
-    pushTail(node) {
-
-        // don't call this on an empty slist - use pushHead
-        node.prev = this.tail;
-        node.next = null;
-        this.tail.next = node;
-        this.tail = node;
-
-    }
-
-    popTail() {
-
-        this.tail = this.tail.prev;
-        this.tail.next = null;
         
+        this._push(initialBodySegment);
+    }
+    
+    updateBodySegmentCoordinates() {
+        this._moveHead();
+        this._moveTail();
+    }
+
+    _moveHead() {
+        var newHeadCoordinates;
+
+        newHeadCoordinates = this.getCurrentFrameHeadCoordinate();
+        this._push(new SnekBodySegment(newHeadCoordinates));
+    }
+
+    getCurrentFrameHeadCoordinate() {
+        var newPosX, newPosY, headCoordinates;
+    
+        headCoordinates = this.head.coordinatePosition;
+        newPosX = headCoordinates.x + speedX;
+        newPosY = headCoordinates.y + speedY;
+
+        return { x: newPosX, y: newPosY };
+    }
+
+    _moveTail() {
+        if (growFramesCounter > 0) {
+            // leaves tail unmoved for growFramesCounter frames, effectively growing the size of the snek
+            growFramesCounter--;
+        } else {
+            this._pop();
+        }
+    }
+
+    _push(newBodySegment) {
+        if (this.head === null) {
+            this._pushToHeadOfEmptySnek(newBodySegment);
+        } else {
+            this._pushToHeadOfNonEmptySnek(newBodySegment);
+        }
+        // this.length
+    }
+
+    _pushToHeadOfEmptySnek(segment) {
+        segment.previousSegment = null;
+        segment.nextSegment = null;
+        this.head = segment;
+        this.tail = segment;
+    }
+
+    _pushToHeadOfNonEmptySnek(segment) {
+        segment.previousSegment = null;
+        segment.nextSegment = this.head;
+        this.head.previousSegment = segment;
+        this.head = segment;
+    }
+
+    _pop() {
+        this.tail = this.tail.previousSegment;
+        this.tail.nextSegment = null;
+    }
+
+}
+
+function updateSnekUI() {
+    snek.updateBodySegmentCoordinates();
+    _drawSnek();
+}
+
+function _drawSnek() {
+    var currentSegment, opts;
+
+    currentSegment = snek.tail;
+    opts = {
+        blockSize: blockSize,
+        lineWidth: '1'
+    };
+
+    while (currentSegment) {
+        _drawSnekSegment(currentSegment, opts);
+        currentSegment = currentSegment.previousSegment;
     }
 }
 
-/******************************************** CODE *********************************************/
-
-function _growSnake() {
-
+function _drawSnekSegment(currentSegment, opts) {
+    opts.fillStyle = _getSnekSegmentFillColor(currentSegment);
+    _paintCanvasCell(currentSegment.coordinatePosition.x, currentSegment.coordinatePosition.y, opts);
 }
 
-function drawSnek() {
-
-    var snekX, snekY, snekHead, snekIter;
-    
-    snekHead = snek.head.value;
-    
-    // The checks below will need to move once we start to add in gameOver phase
-    snekX = (snekHead.x + speedX) > (canvas.width - blockSize) ? 
-        (canvas.width - blockSize) :
-        ((snekHead.x + speedX) < 0) ? 0 : (snekHead.x + speedX);
-    snekY = (snekHead.y + speedY) > (canvas.height - blockSize) ? 
-        (canvas.height - blockSize) :
-        ((snekHead.y + speedY) < 0) ? 0 : (snekHead.y + speedY);
-
-    //var snekX = snekHead.x + speedX;
-    //var snekY = snekHead.y + speedY;
-    snek.pushHead(new SlistNode( { x: snekX, y: snekY } ))
-
-    if (!growCnt) {
-        snek.popTail();
+function _getSnekSegmentFillColor(segment) {
+    if (segment.previousSegment === null) {
+        return snekHeadColor;
+    } else if (segment.nextSegment === null) {
+        return snekTailColor;
     } else {
-        growCnt--;
+        return snekBodyColor;
     }
-
-    snekIter = snek.tail;
-    while (snekIter) {
-        //TODO: refactor/reuse
-        painter.fillStyle = snekIter.prev === null ? snakeFillHead : snekIter.next === null ? snakeFillTail : snakeFill;
-        painter.fillRect(snekIter.value.x, snekIter.value.y, blockSize, blockSize);
-        painter.beginPath();
-        painter.lineWidth = '1';
-        painter.rect(snekIter.value.x, snekIter.value.y, blockSize, blockSize);
-        painter.stroke();
-
-        snekIter = snekIter.prev;
-    }
-
 }
 
-function drawFood() {
+function updateFood() {
+    
+    var opts;
 
-    var rawX, rawY;
-    if (foodEaten) {
-        rawX = Math.random() * (canvas.width - blockSize);
-        food.x = rawX - (rawX % blockSize);
-
-        rawY = Math.floor(Math.random() * (canvas.height - blockSize));
-        food.y = rawY - (rawY % blockSize);
-
-        foodEaten = false;
+    if (isFoodEaten) {
+        food = {food, ..._generateNewFoodCoordinate()};
+        isFoodEaten = false;
     }
 
-    painter.fillStyle = foodFill;
-    painter.fillRect(food.x, food.y, blockSize, blockSize);
+    opts = {
+        fillStyle: foodColor,
+        blockSize: blockSize,
+        lineWidth: '1'
+    };
+
+    _paintCanvasCell(food.x, food.y, opts)
+}
+
+function _generateNewFoodCoordinate() {
+    var randX, randY;
+    
+    randX = Math.random() * (canvas.width - blockSize);
+    randY = Math.random() * (canvas.height - blockSize);
+
+    return { x: (randX - (randX % blockSize)), y: (randY - (randY % blockSize)) };
+}
+
+function _paintCanvasCell(coordX, coordY, opts) {
+    painter.fillStyle = opts.fillStyle;
+    painter.fillRect(coordX, coordY, opts.blockSize, opts.blockSize);
     painter.beginPath();
-    painter.lineWidth = '1';
-    painter.rect(food.x, food.y, blockSize, blockSize);
+    painter.lineWidth = opts.lineWidth;
+    painter.rect(coordX, coordY, opts.blockSize, opts.blockSize);
     painter.stroke();
-
 }
 
-function draw() {
-    
+function updateScore() {
+    $('#score').text(score);
+}
+
+function drawUI() {
     painter.clearRect(0, 0, canvas.width, canvas.height);
-    if (!gameOver) {
-        drawSnek();
-        drawFood();
-    }
-    
+    if (!isGameOver) {
+        updateSnekUI();
+        updateFood();
+        updateScore();
+    } 
 }
 
 function processKeyStroke(e) {
-
     switch (e.key) {
         case 'Up':
         case 'ArrowUp':
         case 'w':
-            if (speedY === 0) {
-                speedY = blockSize * -1;
-                speedX = 0;
-            }
+            _handleUpKeyStroke();
             break;
         
         case 'Down':
         case 'ArrowDown':
         case 's':
-            if (speedY === 0) {
-                speedY = blockSize;
-                speedX = 0;
-            }
+            _handleDownKeyStroke();
             break;
 
         case 'Left':
         case 'ArrowLeft':
         case 'a':
-            if (speedX === 0) {
-                speedX = blockSize * -1;
-                speedY = 0;
-            }
+            _handleLeftKeyStroke();
             break;
 
         case 'Right':
         case 'ArrowRight':
         case 'd':
-            if (speedX === 0) {
-                speedX = blockSize;
-                speedY = 0;
-            }
+            _handleRightKeyStroke();
             break;
     }
+}
 
+function _handleUpKeyStroke() {
+    if (!_isSnekMovingVertically()) {
+        speedY = blockSize * -1;
+        speedX = 0;
+    }
+}
+
+function _handleDownKeyStroke() {
+    if (!_isSnekMovingVertically()) {
+        speedY = blockSize;
+        speedX = 0;
+    }
+}
+
+function _handleLeftKeyStroke() {
+    if (!_isSnekMovingHorizontally()) {
+        speedX = blockSize * -1;
+        speedY = 0;
+    }
+}
+
+function _handleRightKeyStroke() {
+    if (!_isSnekMovingHorizontally()) {
+        speedX = blockSize;
+        speedY = 0;
+    }
+}
+
+function _isSnekMovingVertically() {
+    return speedY !== 0;
+}
+
+function _isSnekMovingHorizontally() {
+    return speedX !== 0;
 }
 
 // Check for collisions with:
@@ -211,48 +263,211 @@ function processKeyStroke(e) {
 //  - wall
 //  - self
 function handleCollisions() {
-    //food
-    if (snek.head.value.x === food.x && snek.head.value.y === food.y) {
-        // newfood
-        foodEaten = true;
-        growCnt = growSize;
+    var headCoordinates = snek.getCurrentFrameHeadCoordinate();
+
+    handleFoodCollisions(headCoordinates);
+    handleWallCollision(headCoordinates);
+    handleSelfCollision(headCoordinates);
+}
+
+function handleFoodCollisions(headCoordinates) {
+    if (_isSnekCollideWithFood(headCoordinates)) {
+        _setBehaviorAfterFoodCollision();
+    }
+}
+
+function _isSnekCollideWithFood(headCoordinates) {
+    return _isSameCoordinate(headCoordinates, food);
+}
+
+function _setBehaviorAfterFoodCollision() {
+    isFoodEaten = true;
+    growFramesCounter = growSegments;
+    _calculateNewScore();
+}
+
+function _calculateNewScore() {
+    score += difficulty/3 ;
+}
+
+function handleWallCollision(headCoordinates) {
+    if (_isSnekCollideWithWall(headCoordinates)) {
+        _setBehaviorAfterWallCollision();
+    }
+}
+
+function _isSnekCollideWithWall(headCoordinates) {
+    return (headCoordinates.x < 0 ||
+            headCoordinates.x === canvas.width ||
+            headCoordinates.y < 0 ||
+            headCoordinates.y === canvas.height);
+}
+
+function _setBehaviorAfterWallCollision() {
+    _invokeEndGame();
+}
+
+function handleSelfCollision(headCoordinates) {
+    if (_isSnekCollideWithSelf(headCoordinates)) {
+        _setBehaviorAfterSelfCollision();
+    }
+}
+
+function _isSnekCollideWithSelf(headCoordinates) {
+    var segmentIter = snek.head.nextSegment;
+
+    while (segmentIter !== null) {
+        if (_isSameCoordinate(headCoordinates, segmentIter.coordinatePosition)) {
+            return true;
+        }
+
+        segmentIter = segmentIter.nextSegment;
     }
 
-    // wall
+    return false;
+}
 
-    // self
+function _setBehaviorAfterSelfCollision() {
+    _invokeEndGame();
+}
+
+function _isSameCoordinate(cell1, cell2) {
+    return (cell1.x === cell2.x && cell1.y === cell2.y);
+}
+
+function _invokeEndGame() {
+    isGameOver = true;
+    resetGame = false;
+    _getPlayAgainFromUser();
+    // alert(`GAME OVER! Your score: ${score}`);
+}
+
+function _getPlayAgainFromUser() {
+    _setupGameOverMenu();
+    _requestPlayAgain();
+}
+
+function _setupGameOverMenu() {
+    $('#final-score').text(score);
+    $('#game-over-menu').show();
+    difficulty = null;
+    document.addEventListener('keydown', processPlayAgainKeyStroke);
+}
+
+function processPlayAgainKeyStroke(e) {
+    switch (e.key) {
+        case 'y':
+            resetGame = true;
+            break;
+        case 'n':
+            resetGame = false;
+            break;
+    }
+}
+
+function _requestPlayAgain() {
+
+    setTimeout(function() {
+        if (resetGame) {
+            document.removeEventListener('keydown', processPlayAgainKeyStroke);
+            init(true);
+        } else {
+            window.requestAnimationFrame(_requestPlayAgain);
+        }
+    }, 1000/120);
 
 }
 
-function run(timestamp) {
+function run() {
     
     setTimeout(function() {
-        handleCollisions();
-        draw();
-        window.requestAnimationFrame(run);
-    }, 1000/24);
+        if (!isGameOver) {
+            handleCollisions();
+            drawUI();
+            window.requestAnimationFrame(run);
+        } else {
+            //init();
+        }
+    }, 1000/difficulty);
 
 }
 
-function init() {
+function init(isReset) {
 
-    var snekNode;
+    if (isReset) {
+        score = 0;
+        $('#game-over-menu').hide();
+    }
 
-    gameOver = false;
     canvas = $("#playArea")[0];
     painter = canvas.getContext("2d");
 
-    food = {
-        x: 180,
-        y: 240
-    }
-    foodEaten = false;
-
-    snekNode = new SlistNode( {x: 140, y: 240} );
-    snek = new SlinkedSlist(snekNode);
     speedX = blockSize;
     speedY = 0;
-    growSize = 2;
+    growSegments = 2;
+    
+    isFoodEaten = false;
+    
+    _getGameDifficultyFromUser();
+}
+
+function _getGameDifficultyFromUser() {
+    this._setupDifficultyMenu();
+    this._requestGameDifficulty();
+}
+
+function _setupDifficultyMenu() {
+    $('#game-menu').show();
+    document.addEventListener('keydown', processDifficultyKeyStroke);
+}
+
+function _requestGameDifficulty() {
+    
+    setTimeout(function() {
+        if (difficulty === null) {
+            window.requestAnimationFrame(_requestGameDifficulty);
+        } else {
+            document.removeEventListener('keydown', processDifficultyKeyStroke);
+            startGame();
+        }
+    }, 1000/120);
+    
+}
+
+function processDifficultyKeyStroke(e) {
+    var easy = $('#difficulty-easy').text();
+    var medium = $('#difficulty-medium').text();
+    var hard = $('#difficulty-hard').text();
+    var insane = $('#difficulty-insane').text();
+    
+    switch (e.key) {
+        case easy:
+            difficulty = LEVEL.EASY;
+            break;
+        case medium:
+            difficulty = LEVEL.MEDIUM;
+            break;
+        case hard:
+            difficulty = LEVEL.HARD;
+            break;
+        case insane:
+            difficulty = LEVEL.INSANE;
+            break;
+    }
+}
+
+function startGame() {
+
+    $('#game-menu').hide();
+    $('#game-over-menu').hide();
+
+    isGameOver = false;
+    food = {
+        x: 240,
+        y: 240
+    }
+    snekNode = new SnekBodySegment({ x: 140, y: 240 });
+    snek = new SnekBody(snekNode);
 
     // Register keystroke behavior
     document.addEventListener('keydown', processKeyStroke);
@@ -260,5 +475,5 @@ function init() {
     run();
 
 }
-
+    
 $(document).ready(init)
